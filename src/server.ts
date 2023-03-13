4663fa32354dd88fdc8c1c9334488056ddd53bdf
Roger Miño <roger.rm03@gmail.com>
2023-03-13 20:00:51 -0300

    probando deploy 5.0

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ export default class Server{
         this.app.use(express.json())
         this.app.use(morgan('dev'))
         this.app.use((req, res, next) => {
-            res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+            res.header('Access-Control-Allow-Origin', 'http://localhost:3000'||"https://kanto-pokedex-client.vercel.app/"); // update to match the domain you will make the request from
             res.header('Access-Control-Allow-Credentials', 'true');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
             res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
