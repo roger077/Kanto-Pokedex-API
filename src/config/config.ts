4db082bf13231017802e85ef1f2122df5ffad877
Roger Miño <roger.rm03@gmail.com>
2023-03-13 20:08:35 -0300

    probando deploy 6.0

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,14 +7,16 @@ const DB_USERNAME = process.env.DB_USERNAME
 const DB_HOST = process.env.DB_HOST;
 const DB_NAME = process.env.DB_NAME
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_PORT = Number(process.env.DB_PORT)
+const DB_PORT = Number(process.env.DB_PORT);
+const FRONT_APP=process.env.FRONT_APP
 export{
     PORT,
     DB_USERNAME,
     DB_HOST,
     DB_NAME,
     DB_PASSWORD,
-    DB_PORT
+    DB_PORT,
+    FRONT_APP
 }
 
 
